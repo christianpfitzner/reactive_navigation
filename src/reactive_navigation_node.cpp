33de0f4490b4c2a85ba062f55aeaf19539f8c72f
Christian Pfitzner <christian.pfitzner@evocortex.com>
2018-06-05 21:13:49 +0200

    -- adding c++11 support -- adding new version of tutorial description -- minor changes in code -- added comments

--- src/reactive_navigation_node.cpp
+++ src/reactive_navigation_node.cpp
@@ -11,23 +11,39 @@
 #include <geometry_msgs/Point.h>
 
 
-sensor_msgs::LaserScan _scan;
-ros::Publisher         _twistPub;
+sensor_msgs::LaserScan _scan;             //!< global variable to store scan message
+ros::Publisher         _twistPub;         //!< global variable to publish twist messages
 
-const double _phi_threshold   = 0.5;
-const double _range_threshold = 1.0;
 
+
+/**
+ *
+ * @param phi
+ * @return
+ */
 float isAngleInDrivingDirection(const double& phi)
 {
    return 1.0;
 }
 
 
+/**
+ * Function to check the weight for distance
+ * This function is used to calculate the alpha angle
+ * @param range      range of a single lidar ray
+ * @return
+ */
 float isDistanceGreat(const double& range)
 {
    return 1.0;
 }
 
+/**
+ * Function to convert a polar coordinate in cartesian coordinate
+ * @param range
+ * @param phi
+ * @return
+ */
 geometry_msgs::Point getCartesianCoordinate(const double& range, const double phi)
 {
    geometry_msgs::Point point;
@@ -36,10 +52,18 @@ geometry_msgs::Point getCartesianCoordinate(const double& range, const double ph
    return point;
 }
 
+
+/**
+ * The function returns true if an obstacle from the lidar scan is within the
+ * current driving direction
+ * @param range
+ * @param phi
+ * @return
+ */
 bool isInDrivingWay(const double& range, const double& phi)
 {
    const geometry_msgs::Point p = getCartesianCoordinate(range, phi);
-   if((::fabs(p.y) < 0.25)) return false;
+   if((::fabs(p.y) > 0.25)) return false;
 
    return true;
 }
@@ -48,7 +72,7 @@ bool isInDrivingWay(const double& range, const double& phi)
 
 
 /**
- * Function to publish velocity
+ * Function to publish velocity message
  */
 void publishTwist(void)
 {
@@ -62,8 +86,11 @@ void publishTwist(void)
   double sum_2    = 0.0;         // denominator of atan2 function used for orientation
   double min_dist = 5;           // variable to control speed
 
+  // implement here the function mentioned in the script with the atan2
   for(unsigned int i=0 ; i<_scan.ranges.size() ; ++i)
   {
+
+
 
 
   }
@@ -87,7 +114,7 @@ void publishTwist(void)
    * Below a given threshold the velocity is set to zero to
    * solve this problem.
    */
-   if (speed < 0.01)                 twist.angular.z = 0.1;
+   if (speed < 0.01)                 twist.linear.x  = 0.0;
    if (fabs(twist.angular.z) < 0.05) twist.angular.z = 0.0;
 
 
@@ -99,8 +126,8 @@ void publishTwist(void)
     * this is necessary so the simulation keeps being close
     * to the real world
     */
-   const float v_max   = 1.0; // define maximum velocity in x direction
-   const float yaw_max = 1.0; // define maximum turn speed in rad
+   constexpr float v_max   = 1.0; // define maximum velocity in x direction
+   constexpr float yaw_max = 1.0; // define maximum turn speed in rad
 
    if(twist.linear.x  > v_max )   twist.linear.x  =  v_max;
    if(twist.linear.x  < -v_max)   twist.linear.x  = -v_max;
@@ -124,17 +151,25 @@ void laserCallback(const sensor_msgs::LaserScan &msg)
    publishTwist();
 }
 
+
+/**
+ * Main function of ros node
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, char **argv)
 {
+   // initialize ros node with name reactive_laser
    ros::init(argc, argv, "reactive_laser");
 
    ros::NodeHandle nh;
 
-   const std::string vel_topic;
-   const std::string laser_topic;
+   const std::string vel_topic   = ("INSERT_TOPIC_FOR_TWIST");
+   const std::string laser_topic = ("INSERT_TOPIC_FOR_LASER");
 
-   _twistPub = nh.advertise < geometry_msgs::Twist > ("robot0/cmd_vel", 1);
-   ros::Subscriber rangeLeftSub = nh.subscribe("robot0/laser_0", 1, laserCallback);
+   _twistPub = nh.advertise < geometry_msgs::Twist > (vel_topic, 1);
+   ros::Subscriber rangeLeftSub = nh.subscribe(       laser_topic, 1, laserCallback);
 
    ros::spin();
 
