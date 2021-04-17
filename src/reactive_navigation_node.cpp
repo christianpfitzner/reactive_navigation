6133bc26a70e0f995011162e2de1f02c5bf5b3e3
Christian Pfitzner <>
2021-04-17 10:53:48 +0200

    -- update

--- src/reactive_navigation_node.cpp
+++ src/reactive_navigation_node.cpp
@@ -32,7 +32,14 @@ ros::Publisher         _twistPub;         //!< global variable to publish twist 
  */
 float isAngleInDrivingDirection(const double& phi)
 {
-   return 1.0;
+   // if(std::abs(phi) < 1)
+   //    return 1.0; 
+   // else  
+   //    return 0.0; 
+   
+   return (0.5 * std::cos(2*phi) + 1);
+   
+   // return 1.0;
 }
 
 
@@ -44,7 +51,17 @@ float isAngleInDrivingDirection(const double& phi)
  */
 float isDistanceGreat(const double& range)
 {
-   return 1.0;
+   // if(range > 1.0)
+   //    return 1.0; 
+   // else
+   //    return 0; 
+
+
+
+   return (1.0 / (1 + std::exp(-2*range + 2)));
+
+
+   // return 1.0;
 }
 
 /**
@@ -91,22 +108,33 @@ void publishTwist(void)
    double speed = 0.0;  // speed in x direction
 
 
-  double sum_1    = 0.0;         // nominator of atan2 function and
-  double sum_2    = 0.0;         // denominator of atan2 function used for orientation
-  double min_dist = 5;           // variable to control speed
+   double sum_1    = 0.0;         // nominator of atan2 function and
+   double sum_2    = 0.0;         // denominator of atan2 function used for orientation
+   double min_dist = 5;           // variable to control speed
 
-  // implement here the function mentioned in the script with the atan2
-  for(unsigned int i=0 ; i<_scan.ranges.size() ; ++i)
-  {
+   // implement here the function mentioned in the script with the atan2
+   for(unsigned int i=0 ; i<_scan.ranges.size() ; ++i)
+   {
+      const auto angle = _scan.angle_min + i* _scan.angle_increment; 
+      const auto dist  = _scan.ranges[i]; 
+
+      sum_1 += std::sin(angle)*isAngleInDrivingDirection(angle)*isDistanceGreat(dist);
+      sum_2 += std::cos(angle)*isAngleInDrivingDirection(angle)*isDistanceGreat(dist); 
+
+      if(isInDrivingWay(dist, angle))
+      {
+         if(dist < min_dist)
+            min_dist = dist; 
+      }
+   }
 
 
-
-
-  }
-
+   constexpr auto robot_radius = 0.3; 
    alpha = ::atan2(sum_1, sum_2);
-//   speed = 1.0;
-   speed = 0.0;
+   speed = min_dist - robot_radius; 
+   
+   
+   // speed = 0.0;
 
 
    geometry_msgs::Twist twist;
@@ -138,10 +166,10 @@ void publishTwist(void)
    constexpr float v_max   = 1.0; // define maximum velocity in x direction
    constexpr float yaw_max = 1.0; // define maximum turn speed in rad
 
-   if(twist.linear.x  > v_max )   twist.linear.x  =  v_max;
-   if(twist.linear.x  < -v_max)   twist.linear.x  = -v_max;
-   if(twist.angular.z > yaw_max)  twist.angular.z =  yaw_max;
-   if(twist.angular.z < -yaw_max) twist.angular.z = -yaw_max;
+   if(twist.linear.x  >  v_max )   twist.linear.x  =  v_max;
+   if(twist.linear.x  < -v_max)    twist.linear.x  = -v_max;
+   if(twist.angular.z >  yaw_max)  twist.angular.z =  yaw_max;
+   if(twist.angular.z < -yaw_max)  twist.angular.z = -yaw_max;
 
    // debug output to console
    ROS_INFO_STREAM("linear  x: " << twist.linear.x);
@@ -174,8 +202,8 @@ int main(int argc, char **argv)
 
    ros::NodeHandle nh;
 
-   const std::string vel_topic   = ("INSERT_TOPIC_FOR_TWIST");
-   const std::string laser_topic = ("INSERT_TOPIC_FOR_LASER");
+   const std::string vel_topic   = ("/robot0/cmd_vel");
+   const std::string laser_topic = ("/robot0/laser_0");
 
    _twistPub = nh.advertise < geometry_msgs::Twist > (vel_topic, 1);
    ros::Subscriber rangeLeftSub = nh.subscribe(       laser_topic, 1, laserCallback);
